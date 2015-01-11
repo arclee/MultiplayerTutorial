57fa20d76c78a105df8966a6618f8f7bb0e29a07
arclee <arclee0117@gmail.com>
2015-01-11 22:49:47 +0800

    gps realtime match

---
+++ Assets/Scripts/GameGPS.cs
@@ -0,0 +1,165 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using GooglePlayGames;
+
+public class GameGPS : MonoBehaviour {
+	
+	public int GUIScreenSizeX = 845;
+	public int GUIScreenSizeY = 480;
+	static public GameGPS Instance;
+
+	static public Action<bool>	onSignIn;
+	static public Action<bool>	onHighScore;
+
+	void Awake()
+	{
+		if (Instance != null)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			Instance = this;
+			DontDestroyOnLoad(transform.gameObject);
+		}		
+	}
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		//PlayGamesPlatform.DebugLogEnabled = true;
+		SignIn();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public void SignIn()
+	{
+		if (IsSignIn())
+		{
+			return;
+		}
+
+		PlayGamesPlatform.Instance.localUser.Authenticate(SignInCB);
+	}
+
+	public void SignInCB(bool success)
+	{
+
+		if (onSignIn != null)
+		{
+			onSignIn(success);
+		}
+		
+	}
+
+	public bool IsSignIn()
+	{
+		return PlayGamesPlatform.Instance.IsAuthenticated();
+	}
+	
+	public void SignOut()
+	{
+		if (!IsSignIn())
+		{
+			return;
+		}
+		PlayGamesPlatform.Instance.SignOut();
+	}
+
+	public void ShowLeaderboardUI()
+	{
+		if (!IsSignIn())
+		{
+			SignIn();
+			return;
+		}
+		PlayGamesPlatform.Instance.ShowLeaderboardUI();
+	}
+	
+	public void ShowAchievementsUI()
+	{
+		if (!IsSignIn())
+		{
+			SignIn();
+			return;
+		}
+
+		PlayGamesPlatform.Instance.ShowAchievementsUI();
+	}
+	
+
+
+	public void HighScore(int score)
+	{
+		if (!IsSignIn())
+		{
+			return;
+		}
+
+		PlayGamesPlatform.Instance.ReportScore((long)score, "CgkIgrHW7YIGEAIQCA", HighScoreCB);
+	}
+	
+	public void HighScoreCB(bool success)
+	{
+		if (!IsSignIn())
+		{
+			return;
+		}
+		
+		if (onHighScore != null)
+		{
+			onHighScore(success);	
+		}
+		
+	}
+
+	public void RealTimeMatchInvitationScreen()
+	{
+		const int MinOpponents = 1, MaxOpponents = 3;
+		const int GameVariant = 0;
+		PlayGamesPlatform.Instance.RealTime.CreateWithInvitationScreen(MinOpponents, MaxOpponents,
+		                                                               GameVariant, new GameGPSRTInvListener());
+	}
+
+	
+	public void RealTimeMatchQuickGame()
+	{
+		const int MinOpponents = 1, MaxOpponents = 3;
+		const int GameVariant = 1;
+		PlayGamesPlatform.Instance.RealTime.CreateQuickGame(MinOpponents, MaxOpponents,
+		                                                    GameVariant, new GameGPSRTInvListener());
+	}
+
+	
+	public void RealTimeMatchLeaveRoom()
+	{
+		PlayGamesPlatform.Instance.RealTime.LeaveRoom();
+	}
+	void OnGUI()
+	{
+		Matrix4x4 _matrix = GUI.matrix;
+		_matrix.m00 = (float)Screen.width / GUIScreenSizeX;
+		_matrix.m11 = (float)Screen.height / GUIScreenSizeY;
+		GUI.matrix = _matrix;
+
+		if (GUI.Button(new Rect (0, 100, 200, 50), "RealTimeMatchInvitationScreen"))
+		{
+			RealTimeMatchInvitationScreen();
+		}
+		if (GUI.Button(new Rect (0, 150, 200, 50), "RealTimeMatchQuickGame"))
+		{
+			RealTimeMatchQuickGame();
+		}
+		if (GUI.Button(new Rect (0, 200, 200, 50), "RealTimeMatchLeaveRoom"))
+		{
+			RealTimeMatchLeaveRoom();
+		}		
+	}
+}
---
+++ Assets/Scripts/GameGPSRTInvListener.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using GooglePlayGames.BasicApi.Multiplayer;
+
+public class GameGPSRTInvListener : RealTimeMultiplayerListener
+{
+	
+	/// <summary>
+	/// Called during room setup to notify of room setup progress.
+	/// </summary>
+	/// <param name="percent">The room setup progress in percent (0.0 to 100.0).</param>
+	void RealTimeMultiplayerListener.OnRoomSetupProgress(float percent)
+	{
+		Debug.Log("OnRoomSetupProgress:" + percent);
+	}
+	
+	/// <summary>
+	/// Notifies that room setup is finished. If <c>success == true</c>, you should
+	/// react by starting to play the game; otherwise, show an error screen.
+	/// </summary>
+	/// <param name="success">Whether setup was successful.</param>
+	void RealTimeMultiplayerListener.OnRoomConnected(bool success)
+	{
+		
+		Debug.Log("OnRoomConnected:" + success);
+	}
+
+	
+	/// <summary>
+	/// Notifies that the current player has left the room. This may have happened
+	/// because you called LeaveRoom, or because an error occurred and the player
+	/// was dropped from the room. You should react by stopping your game and
+	/// possibly showing an error screen (unless leaving the room was the player's
+	/// request, naturally).
+	/// </summary>
+	void RealTimeMultiplayerListener.OnLeftRoom()
+	{
+		Debug.Log("OnLeftRoom:");
+		
+	}
+
+	
+	/// <summary>
+	/// Called when peers connect to the room.
+	/// </summary>
+	/// <param name="participantIds">Participant identifiers.</param>
+	void RealTimeMultiplayerListener.OnPeersConnected(string[] participantIds)
+	{
+		for (int i = 0; i < participantIds.Length; i++)
+		{
+			Debug.Log("OnPeersConnected:" + participantIds[i]);
+
+		}
+	}
+
+	
+	/// <summary>
+	/// Called when peers disconnect from the room.
+	/// </summary>
+	/// <param name="participantIds">Participant identifiers.</param>
+	void RealTimeMultiplayerListener.OnPeersDisconnected(string[] participantIds)
+	{
+		for (int i = 0; i < participantIds.Length; i++)
+		{
+			Debug.Log("OnPeersDisconnected:" + participantIds[i]);
+			
+		}
+		
+	}
+
+	
+	/// <summary>
+	/// Called when a real-time message is received.
+	/// </summary>
+	/// <param name="isReliable">Whether the message was sent as a reliable message or not.</param>
+	/// <param name="senderId">Sender identifier.</param>
+	/// <param name="data">Data.</param>
+	void RealTimeMultiplayerListener.OnRealTimeMessageReceived(bool isReliable, string senderId, byte[] data)
+	{
+		Debug.Log("OnRealTimeMessageReceived:" + " isReliable:" + isReliable + " senderId:"+senderId
+		          + data);
+		
+	}
+
+
+
+}
