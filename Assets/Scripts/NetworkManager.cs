7a916a4f28133d855b0a758a7603bd4dda603104
arclee <arclee0117@gmail.com>
2015-01-11 15:55:46 +0800

    加入多種連線方式. IP、GUID(to NAT)

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -17,11 +17,30 @@ public class NetworkManager : MonoBehaviour
 
 	public string proxyIP = "1.1.1.1";
 	public string proxyPort = "1111";
+	
+	public string serverPassword = "asdf123";
+	public string serverIP = "1.1.1.1";
+	public string serverPort = "1234";
+	public string toGuid = "1234";
 
+	public int GUIScreenSizeX = 845;
+	public int GUIScreenSizeY = 480;
+
+	public TextMesh debugText; 
+	
+	//php check.	
+	string phpret = "";
+	public string phpurl = "https://arcleebs.appspot.com/getip";
+	public string localhost = "http://localhost/getip";
+	public bool uselocalhost = false;
 	void Start()
 	{
 		SetProxyData();
 		MasterServer.UnregisterHost();
+
+		StartCoroutine("GetMyIP");
+
+
 	}
 
 	void SetProxyData()
@@ -37,22 +56,67 @@ public class NetworkManager : MonoBehaviour
 
     void OnGUI()
     {
+		Matrix4x4 _matrix = GUI.matrix;
+		_matrix.m00 = (float)Screen.width / GUIScreenSizeX;
+		_matrix.m11 = (float)Screen.height / GUIScreenSizeY;
+		GUI.matrix = _matrix;
 		
+		/*
 		proxyIP = GUI.TextField(new Rect(100, 0, 250, 50), proxyIP);
 		proxyPort = GUI.TextField(new Rect(100, 50, 250, 50), proxyPort);
+*/
+		
+		serverIP = GUI.TextField(new Rect(100, 0, 250, 50), serverIP);
+		serverPort = GUI.TextField(new Rect(100, 50, 250, 50), serverPort);
 
+		toGuid = GUI.TextField(new Rect(100, 100, 250, 50), toGuid);
+/*
 		if (GUI.Button(new Rect(100, 100, 250, 50), "SetProxyData"))
 		{
 			SetProxyData();
 		}
-
+*/
         if (!Network.isClient && !Network.isServer)
         {
             if (GUI.Button(new Rect(100, 200, 250, 50), "Start Server"))
+			{
                 StartServer();
+			}
 
             if (GUI.Button(new Rect(100, 250, 250, 50), "Refresh Hosts"))
+			{
                 RefreshHostList();
+			}
+			
+			if (GUI.Button(new Rect(100, 300, 250, 50), "Create Server"))
+			{
+				//Network.incomingPassword = serverPassword;
+				bool useNat = !Network.HavePublicAddress();
+				int sp = 0;
+				int.TryParse(serverPort, out sp);
+				Network.InitializeServer(10, sp, useNat);
+
+			}
+			
+			if (GUI.Button(new Rect(100, 350, 250, 50), "Connect Server"))
+			{
+				//bool useNat = !Network.HavePublicAddress();
+				Network.useProxy = isServerUseProxy;
+				int sp = 0;
+				int.TryParse(serverPort, out sp);
+				Network.Connect(serverIP, sp);
+
+			}
+			
+			if (GUI.Button(new Rect(100, 400, 250, 50), "Connect GUID"))
+			{
+				//bool useNat = !Network.HavePublicAddress();
+				Network.useProxy = isServerUseProxy;
+				int sp = 0;
+				int.TryParse(serverPort, out sp);
+				Network.Connect(toGuid);
+				
+			}
 
             if (hostList != null)
             {
@@ -68,13 +132,24 @@ public class NetworkManager : MonoBehaviour
     private void StartServer()
     {
 		Network.useProxy = isServerUseProxy;
-        Network.InitializeServer(5, 25000, !Network.HavePublicAddress());
+		int sp = 0;
+		int.TryParse(serverPort, out sp);
+		Network.InitializeServer(5, sp, !Network.HavePublicAddress());
         MasterServer.RegisterHost(typeName, gameName);
     }
 
     void OnServerInitialized()
     {
 		SpawnPlayer("Player_S");
+
+		
+		debugText.text = phpret
+			+ "\n"
+				+ Network.player.externalIP
+				+ "\n"
+				+ Network.player.ipAddress
+				+ "\n"
+				+ Network.player.port + " "+ Network.HavePublicAddress();
     }
 
 
@@ -85,6 +160,7 @@ public class NetworkManager : MonoBehaviour
             isRefreshingHostList = false;
             hostList = MasterServer.PollHostList();
         }
+
     }
 
     private void RefreshHostList()
@@ -95,12 +171,28 @@ public class NetworkManager : MonoBehaviour
             MasterServer.RequestHostList(typeName);
         }
     }
-
-
+	void OnPlayerConnected(NetworkPlayer player)
+	{
+		debugText.text = "Player connected from " + player.ipAddress + ":" + player.port;
+		MasterServer.UnregisterHost();
+	}
+	void OnFailedToConnect(NetworkConnectionError error)
+	{
+		debugText.text = "Could not connect to server: " + error;
+	}
     private void JoinServer(HostData hostData)
 	{
 		Network.useProxy = isClientUseProxy;
         Network.Connect(hostData);
+		string tmpIp = "";
+		int i = 0;
+		while (i < hostData.ip.Length)
+		{
+			tmpIp = hostData.ip[i] + " ";
+			i++;
+		}
+
+		debugText.text = "Join:" + tmpIp + ":" + hostData.port;
     }
 
     void OnConnectedToServer()
@@ -108,10 +200,74 @@ public class NetworkManager : MonoBehaviour
         SpawnPlayer("Player_C");
     }
 
-
     private void SpawnPlayer(string name)
     {
 		GameObject go = (GameObject)Network.Instantiate(playerPrefab, Vector3.up * 5, Quaternion.identity, 0);
 		go.name = name;
     }
+
+
+	
+	public IEnumerator GetMyIP()
+	{
+		var form = new WWWForm();
+		
+		string url;
+		
+		if (uselocalhost)
+		{
+			url = localhost;
+		}
+		else
+		{
+			url = phpurl;
+		}
+				
+		WWW w = new WWW(url);
+		yield return w;
+		
+		
+		if (w.error != null)
+		{			
+			Debug.Log("server down " + url);
+			debugText.text = "getip server down " + url + " " + w.error;
+
+			
+			debugText.text += phpret
+				+ "\n"
+					+ Network.player.externalIP
+					+ "\n"
+					+ Network.player.externalPort
+					+ "\n"
+					+ Network.player.ipAddress
+					+ "\n"
+					+ Network.player.port + " "+ Network.HavePublicAddress();
+		}
+		else
+		{
+			phpret = w.text;
+			bool success = false;
+			if (phpret == "OK")
+			{
+				success = true;
+			}
+			else
+			{
+				success = false;
+				
+			}
+			
+			debugText.text = phpret
+				+ "\nei"
+					+ Network.player.externalIP
+					+ "\nep:"
+					+ Network.player.externalPort
+					+ "\n:"
+					+ Network.player.ipAddress
+					+ "\n"
+					+ Network.player.port + " "+ Network.HavePublicAddress()
+					+ "\n"
+					+ Network.player.guid;
+		}
+	}
 }
